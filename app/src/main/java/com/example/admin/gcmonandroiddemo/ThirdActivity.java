6c66da3f4e381823130fe8fb5778d5dd43be4917
ramasamy11 <ramasamy.vcp@gmail.com>
2015-12-20 14:00:39 +0530

    Initial commit

---
+++ app/src/main/java/com/example/admin/gcmonandroiddemo/ThirdActivity.java
@@ -0,0 +1,15 @@
+package com.example.admin.gcmonandroiddemo;
+
+/**
+ * Created by Admin on 12/20/2015.
+ */
+import  android.app.Activity;
+import android.os.Bundle;
+
+public class ThirdActivity extends  Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
